5c382ba8e0c7aae19fe6334dce4c0bf949c42a05
Ritvik Singh Chauhan <ritvik.singh.chauhan@gmail.com>
2023-07-19 18:44:22 +0530

    Initializing JAVA-Gradle Project for Monte Carlo Simulation

---
+++ src/main/java/com/ritvik/monte/carlo/simulation/pi/Main.java
@@ -0,0 +1,7 @@
+package com.ritvik.monte.carlo.simulation.pi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=: JAVA Project for Estimating Pi Using the Monte Carlo Simulation Method :=");
+    }
+}
